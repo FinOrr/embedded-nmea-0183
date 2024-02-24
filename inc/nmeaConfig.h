4882222c8616fcc8be1b4ec05e8d6f3b54b373f6
Fin Orr <finorr3@gmail.com>
2024-02-24 08:52:10 +0700

    New config file allows easy enable/disable of required sentences

---
+++ inc/nmeaConfig.h
@@ -0,0 +1,17 @@
+#ifndef INC_NMEA_CONFIG_H_
+#define INC_NMEA_CONFIG_H_
+
+#define SENTENCE_AAM_ENABLED true
+#define SENTENCE_ABK_ENABLED true
+#define SENTENCE_ABM_ENABLED true
+#define SENTENCE_ACA_ENABLED true
+#define SENTENCE_ACK_ENABLED true
+#define SENTENCE_ACN_ENABLED true
+#define SENTENCE_ACS_ENABLED true
+#define SENTENCE_AIR_ENABLED true
+#define SENTENCE_AKD_ENABLED true
+#define SENTENCE_ALA_ENABLED true
+#define SENTENCE_ALC_ENABLED true
+#define SENTNECE_ALF_ENABLED true
+
+#endif
